a61c5c591d6a57095100959e424f501a36564b44
arimaxx <155280132+arimaxx@users.noreply.github.com>
2024-01-15 07:25:37 +0530

    Update queue.py

--- AarohiX/utils/inline/queue.py
+++ AarohiX/utils/inline/queue.py
@@ -25,10 +25,10 @@ def queue_markup(
         ],
          [
             InlineKeyboardButton(
-                text="🥀 ᴏᴡɴᴇʀ 🥀", url=f"tg://openmessage?user_id={OWNER_ID}",
+                text="🤍 ᴏᴡɴᴇʀ 🤍", url=f"tg://openmessage?user_id={OWNER_ID}",
             ),
             InlineKeyboardButton(
-                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url=SUPPORT_CHAT,
+                text="💫 sᴜᴩᴩᴏʀᴛ 💫", url=SUPPORT_CHAT,
             ),
         ],
     ]
@@ -51,10 +51,10 @@ def queue_markup(
         ],
          [
             InlineKeyboardButton(
-                text="🥀 ᴏᴡɴᴇʀ 🥀", url=f"tg://openmessage?user_id={OWNER_ID}",
+                text="🤍 ᴏᴡɴᴇʀ 🤍", url=f"tg://openmessage?user_id={OWNER_ID}",
             ),
             InlineKeyboardButton(
-                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url=SUPPORT_CHAT,
+                text="💫 sᴜᴩᴩᴏʀᴛ 💫", url=SUPPORT_CHAT,
             ),
         ],
     ]
@@ -98,10 +98,10 @@ def aq_markup(_, chat_id):
         ],
          [
             InlineKeyboardButton(
-                text="🥀 ᴏᴡɴᴇʀ 🥀", url=f"tg://openmessage?user_id={OWNER_ID}",
+                text="🤍 ᴏᴡɴᴇʀ 🤍", url=f"tg://openmessage?user_id={OWNER_ID}",
             ),
             InlineKeyboardButton(
-                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url=SUPPORT_CHAT,
+                text="💫 sᴜᴩᴩᴏʀᴛ 💫", url=SUPPORT_CHAT,
             ),
         ],
     ]
