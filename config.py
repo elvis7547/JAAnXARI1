a3c58bdb42b8f41fc15ed7803c3c5144b4ec5fce
arimaxx <155280132+arimaxx@users.noreply.github.com>
2024-02-05 07:12:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/arimaxx/JAAnXARI",
+    "https://github.com/stkeditz/AAROHIxMUSICv2",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
