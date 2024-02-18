ff80c3e0f70bf1f6116a92f17d30ebcd37fd07b6
arimaxx <155280132+arimaxx@users.noreply.github.com>
2024-02-18 10:06:48 +0530

    Update couples.py

--- AarohiX/plugins/maxa/couples.py
+++ AarohiX/plugins/maxa/couples.py
@@ -88,7 +88,7 @@ async def ctest(_, message):
 
          draw = ImageDraw.Draw(img)
 
-         img.paste(img1, (100, 140), img1)
+         img.paste(img1, (85, 140), img1)
          img.paste(img2, (789, 160), img2)
 
          img.save(f'test_{cid}.png')
